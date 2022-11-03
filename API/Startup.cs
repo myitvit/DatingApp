72b80c9c25b38e550fb123cd1d0e8de5cf1dd610
Vitali Prakapenka <myitvit@gmail.com>
2022-11-03 19:21:49 +0300

    Added HTTPS in angular

--- API/Startup.cs
+++ API/Startup.cs
@@ -48,7 +48,7 @@ namespace API
 
             app.UseRouting();
 
-            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
