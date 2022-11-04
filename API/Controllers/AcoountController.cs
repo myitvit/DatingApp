8cf61eb7f9a2ae09b4e8695c19bdabd8cd8347ff
Vitali Prakapenka <myitvit@gmail.com>
2022-11-04 18:33:47 +0300

    Added login endpoint

--- API/Controllers/AcoountController.cs
+++ API/Controllers/AcoountController.cs
@@ -20,7 +20,8 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
-            if (await UserExists(registerDTO.Username)) { 
+            if (await UserExists(registerDTO.Username))
+            {
                 return BadRequest("Username already exists");
             }
 
@@ -36,6 +37,31 @@ namespace API.Controllers
             _context.Users.Add(user);
             // Save changes to the DB
             await _context.SaveChangesAsync();
+
+            return user;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(user => user.UserName == loginDTO.Username);
+
+            if (user == null)
+            {
+                return Unauthorized("Invalid username");
+            }
+
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+
+            var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDTO.Password));
+
+            for (int i = 0; i < computedHash.Length; i++)
+            {
+                if (computedHash[i] != user.PasswordHash[i])
+                {
+                    return Unauthorized("Invalid password");
+                }
+            }
 
             return user;
         }
