662bf8ac4da84f8e2a7ee35f7256c23290dddd9b
Vitali Prakapenka <myitvit@gmail.com>
2022-11-04 16:42:09 +0300

    Added reg DTO and user exists check

--- API/Controllers/AcoountController.cs
+++ API/Controllers/AcoountController.cs
@@ -4,6 +4,7 @@ using API.Entities;
 using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
+using API.DTOs;
 
 namespace API.Controllers
 {
@@ -17,14 +18,18 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
+            if (await UserExists(registerDTO.Username)) { 
+                return BadRequest("Username already exists");
+            }
+
             using var hmac = new HMACSHA512();
 
             var user = new AppUser()
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDTO.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                 PasswordSalt = hmac.Key
             };
             // Add user to the EF
@@ -33,6 +38,11 @@ namespace API.Controllers
             await _context.SaveChangesAsync();
 
             return user;
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(user => user.UserName == username.ToLower());
         }
     }
 }
