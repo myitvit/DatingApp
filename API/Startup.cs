a625411d06f3040716a9ce1da562eb89ba7fcb9d
Vitali Prakapenka <myitvit@gmail.com>
2022-11-05 10:18:43 +0300

    Added TokenService

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,7 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<ITokenService, TokenService>();
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
