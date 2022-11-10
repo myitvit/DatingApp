aadcfedf6c38cc2eb1fbe83a7eaf91b54e0356b8
Vitali Prakapenka <myitvit@gmail.com>
2022-11-10 12:20:24 +0300

    Added a register form

---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  model: any = {};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  register(): void {
+    console.log(this.model);
+  }
+
+  cancel(): void {
+    console.log('Cancelled');
+  }
+}
