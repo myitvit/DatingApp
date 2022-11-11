40d275611304c4707c8a1ce71dd8c45cf308f8fe
Vitali Prakapenka <myitvit@gmail.com>
2022-11-11 15:50:47 +0300

    Parent to child angular componet communication

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   registerMode: boolean = false;
+  users: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.getUsers();
   }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  getUsers(){
+    this.http.get('https://localhost:5001/api/users').subscribe(users=> this.users = users );
   }
 }
