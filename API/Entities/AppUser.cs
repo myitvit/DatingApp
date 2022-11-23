3233ab3a58c8b1a5035a041bbfc0174a46bda913
Vitali Prakapenka <myitvit@gmail.com>
2022-11-23 21:40:52 +0300

    Extended the user entity

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,22 @@
         public byte[] PasswordHash { get; set; }
 
         public byte[] PasswordSalt { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string KnownAs { get; set; }
+
+        public DateTime Created { get; set; } = DateTime.Now;
+
+        public DateTime LastActive { get; set; } = DateTime.Now;
+
+        public string Gender { get; set; }
+
+        public string Inroduction { get; set; }
+        public string LookingFor { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        public ICollection<Photo> Protos { get; set; }
     }
 }
