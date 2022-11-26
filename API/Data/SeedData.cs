c5b0b9bccc5f30ee43579c71684fd1866b53d187
Vitali Prakapenka <myitvit@gmail.com>
2022-11-26 13:46:06 +0300

    Added Seed Data from json file

---
+++ API/Data/SeedData.cs
@@ -0,0 +1,37 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public static class SeedData
+    {
+        public static async Task SeedUsers(DataContext context)
+        {
+            if (await context.Users.AnyAsync())
+            {
+                return;
+            }
+
+            const string fileName = "Data/UserSeedData.json";
+            using FileStream openStream = File.OpenRead(fileName);
+
+            var users = await JsonSerializer.DeserializeAsync<List<AppUser>>(openStream);
+
+            foreach (var user in users)
+            {
+                using var hmac = new HMACSHA512();
+
+                user.UserName = user.UserName.ToLower();
+                user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Password"));
+                user.PasswordSalt = hmac.Key;
+
+                context.Users.Add(user);
+            }
+
+            await context.SaveChangesAsync();
+        }
+    }
+}
