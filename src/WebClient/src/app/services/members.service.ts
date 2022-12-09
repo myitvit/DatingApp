f01e50368277a515f8f2329803e582b9833ec89a
Vitali Prakapenka <myitvit@gmail.com>
2022-12-09 14:13:28 +0300

    Displaying list of members

--- src/WebClient/src/app/services/members.service.ts
+++ src/WebClient/src/app/services/members.service.ts
@@ -7,7 +7,7 @@ import { Member } from 'src/app/models/member';
 // Temporary hack
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
 }
 
