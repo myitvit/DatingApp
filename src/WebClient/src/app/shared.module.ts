8000ae1739ecb58e6097691b375e7eee9dfeeff0
Vitali Prakapenka <myitvit@gmail.com>
2022-12-24 13:14:41 +0300

    Added member card details and tabs

--- src/WebClient/src/app/shared.module.ts
+++ src/WebClient/src/app/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { ToastrModule } from 'ngx-toastr';
+import { TabsModule } from 'ngx-bootstrap/tabs'
 
 @NgModule({
   declarations: [],
@@ -11,10 +12,12 @@ import { ToastrModule } from 'ngx-toastr';
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right'
     }),
+    TabsModule.forRoot(),
   ],
   exports: [
     BsDropdownModule,
-    ToastrModule
+    ToastrModule,
+    TabsModule
   ]
 })
 
