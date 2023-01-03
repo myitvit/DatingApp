bd5ae243c6240c5cd16e965c3b6b3d2a33853c4b
Vitali Prakapenka <myitvit@gmail.com>
2023-01-03 10:16:55 +0300

    Added Photo Service

--- src/api/Extensions/ApplicationServiceExtensions.cs
+++ src/api/Extensions/ApplicationServiceExtensions.cs
@@ -2,7 +2,6 @@ using API.Interfaces;
 using API.Services;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
-using AutoMapper;
 using API.Helpers;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
@@ -16,6 +15,8 @@ namespace API.Extensions
             services.Configure<CloudinarySettings>(configuration.GetSection("CloudinarySettings"));
 
             services.AddScoped<ITokenService, TokenService>();
+
+            services.AddScoped<IPhotoService, PhotoService>();
 
             services.AddScoped<IUserRepository, UserRepository>();
 
