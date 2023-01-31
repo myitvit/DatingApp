8d477490a9a3cc0902d36405048ab34ce292f06a
Vitali Prakapenka <myitvit@gmail.com>
2023-01-31 12:20:58 +0300

    Added register form validators

--- src/UI/src/app/register/register.component.ts
+++ src/UI/src/app/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
@@ -22,11 +22,39 @@ export class RegisterComponent implements OnInit {
   initializeForm(): void {
     this.registerForm = new FormGroup(
       {
-        username: new FormControl(),
-        password: new FormControl(),
-        confirmPassword: new FormControl(),
+        username: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(4)
+          ]
+        ),
+        password: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(24),
+          ]
+        ),
+        confirmPassword: new FormControl('',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.maxLength(24),
+            this.matchValues('password')
+          ]
+        ),
       }
-    )
+    );
+
+    this.registerForm.controls.password.valueChanges.subscribe(() => {
+      this.registerForm.controls.confirmPassword.updateValueAndValidity();
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control?.value === control?.parent?.controls[matchTo].value ? null : { isMatching: true };
+    }
   }
 
   register(): void {
